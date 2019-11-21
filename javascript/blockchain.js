c23ae4145acd8300fc44101b1c19e87549b334d0
Adriana <56120914+dricalopes@users.noreply.github.com>
2019-11-21 20:02:59 -0300

    Add files via upload

--- javascript/blockchain.js
+++ javascript/blockchain.js
@@ -1,4 +1,4 @@
-var enderecoContrato = "0x53C34f4897a4111f9299a9b734aD1d5c380a8Ce5";
+var enderecoContrato = "0x1Ce4B811b401eAA08907620977a3a95B9C064968";
 var provedor = new ethers.providers.Web3Provider(web3.currentProvider);
 ethereum.enable();
 var signatario = provedor.getSigner();
